12de36e1b5df03629683ce54d4d8443f86890741
Tanao <403897983@qq.com>
2022-01-06 22:37:39 +0800

    while

--- 第七章：用户输入和while循环/practice 7-1.py
+++ 第七章：用户输入和while循环/practice 7-1.py
@@ -1,2 +1,2 @@
-message = input('What kind of car do you want to lease?')
+message = input('What kind of car do you want to lease?')   #输入subaru
 print(f'Let me see if I can find you a {message.title()}.')
