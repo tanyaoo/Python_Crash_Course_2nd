9b4dc75b0294ed00263f4544f09fd1d16ebb8c31
Tanao <403897983@qq.com>
2022-03-10 21:01:17 +0800

    添加注册页面

--- learning_blog/users/views.py
+++ learning_blog/users/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+
+
+def register(request):
+    """注册新用户"""
+    if request.method != 'POST':
+        # 显示空的注册列表
+        form = UserCreationForm()
+    else:
+        # 处理填写好的表单
+        form = UserCreationForm(data=request.POST)
+
+        if form.is_valid():
+            new_user = form.save()
+            # 让新用户自动登录
+            login(request, new_user)
+            return redirect('learning_logs:index')
+
+    # 显示空表单或指出表单无效
+    context = {'form': form}
+    return render(request, 'registration/register.html', context)
+
+
